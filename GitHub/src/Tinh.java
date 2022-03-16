ba5907c8e95a1d3017556758a9750be4ae35d353
Vang_15 <Vang_15@DESKTOP-NR54H9D>
2022-03-16 22:02:52 +0700

    Sửa lần 2

--- GitHub/src/Tinh.java
+++ GitHub/src/Tinh.java
@@ -1,6 +1,6 @@
  public tinh() {
-        a = 1;
-        b = 1;
+        a = 0;
+        b = 0;
     }
     public tinh(double a, double b) {
         this.a = a;
@@ -12,7 +12,3 @@
         System.out.print("\nEnter Second Number: ");
         b = sc.nextDouble();
     }
-    System.out.print("\nSum:"+(a+b));
-    System.out.print("\nDifference:"+(a-b));
-    System.out.print("\nProduct:"+(a*b));
-    System.out.print("\nQuotient:"+(a/b));
